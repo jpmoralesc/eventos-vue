f4e76bc5206b214fb1f4564d5f1fa473ab548247
jpmorales <jpmorales@yahoo.com>
2021-10-10 22:33:30 -0500

    Mensaje

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Pedidos from '../views/pedidos.vue'
 
 const routes = [
   {
@@ -14,7 +15,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/pedidos',
+    name: 'Pedidos',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/pedidos.vue')
   }
+
 ]
 
 const router = createRouter({
